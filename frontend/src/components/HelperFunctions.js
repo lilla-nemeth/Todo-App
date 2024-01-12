e6885028f2ad21e7c259431f2398fa5d01c79295
Lilla Németh <naemly@protonmail.com>
2024-01-12 13:41:20 +0200

    moving selectToEdit helper fn to HelperFunctions.js

--- frontend/src/components/HelperFunctions.js
+++ frontend/src/components/HelperFunctions.js
@@ -17,3 +17,17 @@ export function clearError() {
     clearTimeout(timeOut);
 }
 
+export function selectToEdit(
+    el, 
+    editedId, 
+    idSetter, 
+    inputSetter
+) {
+    if (el && editedId === el.id) {
+        idSetter(null);
+        inputSetter('');
+    } else {
+        idSetter(el.id);
+        inputSetter(el.title);
+    }
+}
