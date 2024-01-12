ac1e71a53487404041ecefe45f97bd03df66b21c
Lilla Németh <naemly@protonmail.com>
2024-01-12 22:59:22 +0200

    addTodo part in ToDoInput.js

--- frontend/src/components/ToDoInput.js
+++ frontend/src/components/ToDoInput.js
@@ -1,48 +1,40 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { handleError, handleChange } from '../utils/HelperFunctions';
+import { createOptions } from '../context/RequestOptions';
+import { changeOrGetData } from '../context/Requests';
 
 export default function ToDoInput(props) {
-    const { getAllTodos, token } = props;
+	const { getAllTodos, token } = props;
 
-    const [input, setInput] = useState('');
-    const [errorMsg, setErrorMsg] = useState('');
+	const [input, setInput] = useState('');
+	const [errorMsg, setErrorMsg] = useState('');
 
-    function handleSubmit(event) {
-        event.preventDefault();
+	function handleSubmit(event) {
+		event.preventDefault();
 
-        let options = {
-            method: 'post',
-            url: '/todos',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-auth-token': token,
-            },
-            data: {
-                title: input,
-            },
-        };
-        if (input != '') {
-            axios(options)
-                .then((res) => getAllTodos())
-                .catch((err) => handleError(err, setErrorMsg));
-        }
-        setInput('');
-    }
+		const options = createOptions('post', '/todos', 'cors', 'application/json', token, { title: input });
 
-    return (
-        <div>
-            <form className="form" onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Add todo"
-                    value={input}
-                    className="todoInput"
-                    onChange={(e) => handleChange(e, setInput)}
-                />
-                <p className="errorMsg">{errorMsg}</p>
-            </form>
-        </div>
-    );
+		if (input !== '') {
+			changeOrGetData({
+				options,
+				successCb: (res) => {
+					getAllTodos();
+				},
+				errorCb: (err) => {
+					handleError(err, setErrorMsg);
+				},
+			});
+		}
+		setInput('');
+	}
+
+	return (
+		<div>
+			<form className='form' onSubmit={handleSubmit}>
+				<input type='text' placeholder='Add todo' value={input} className='todoInput' onChange={(e) => handleChange(e, setInput)} />
+				<p className='errorMsg'>{errorMsg}</p>
+			</form>
+		</div>
+	);
 }
