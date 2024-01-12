c107822a81551e3ddb30a762a28f12c2b5064ee6
Lilla Németh <naemly@protonmail.com>
2024-01-12 14:34:41 +0200

    Creating login-signup folder for Login and SignUp components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ToDo from './components/ToDo';
-import SignUp from './components/SignUp';
-import Login from './components/Login';
+import SignUp from './components/login-signup/SignUp';
+import Login from './components/login-signup/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './App.css';
@@ -9,66 +9,55 @@ import axios from 'axios';
 import { handleError, handleLogOut } from './utils/HelperFunctions';
 
 function App() {
-  const [token, setToken] = useState(null);
-  const [user, setUser] = useState('');
-  const [errorMsg, setErrorMsg] = useState('');
+	const [token, setToken] = useState(null);
+	const [user, setUser] = useState('');
+	const [errorMsg, setErrorMsg] = useState('');
 
-  useEffect(() => {
-    let tokenFromLocalStorage = localStorage.getItem('token');
+	useEffect(() => {
+		let tokenFromLocalStorage = localStorage.getItem('token');
 
-    if (tokenFromLocalStorage) {
-      setToken(tokenFromLocalStorage);
+		if (tokenFromLocalStorage) {
+			setToken(tokenFromLocalStorage);
 
-      let options = {
-        method: 'get',
-        url: '/user',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-auth-token': token,
-        },
-      };
-      axios(options)
-        .then((res) => setUser(res.data))
-        .catch((err) => handleError(err, setErrorMsg));
-    }
-  });
+			let options = {
+				method: 'get',
+				url: '/user',
+				mode: 'cors',
+				headers: {
+					'Content-Type': 'application/json',
+					'x-auth-token': token,
+				},
+			};
+			axios(options)
+				.then((res) => setUser(res.data))
+				.catch((err) => handleError(err, setErrorMsg));
+		}
+	});
 
-  if (!token) {
-    return (
-      <>
-        <BrowserRouter>
-          <Routes>
-            <Route 
-              path='/signup' 
-              element={<SignUp />}
-            >
-            </Route>
-            <Route
-              path='/login'
-              element={<Login setToken={setToken} />}
-            ></Route>
-            <Route path='*' element={<Login setToken={setToken} />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </>
-    );
-  }
-  return (
-    <>
-      <BrowserRouter>
-        <Navbar 
-          handleLogOut={handleLogOut} 
-          setToken={setToken} 
-          user={user} 
-        />
-        <Routes>
-          <Route exact path='/' element={<ToDo token={token} />}></Route>
-          <Route path='*' element={<ToDo token={token} />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+	if (!token) {
+		return (
+			<>
+				<BrowserRouter>
+					<Routes>
+						<Route path='/signup' element={<SignUp />}></Route>
+						<Route path='/login' element={<Login setToken={setToken} />}></Route>
+						<Route path='*' element={<Login setToken={setToken} />}></Route>
+					</Routes>
+				</BrowserRouter>
+			</>
+		);
+	}
+	return (
+		<>
+			<BrowserRouter>
+				<Navbar handleLogOut={handleLogOut} setToken={setToken} user={user} />
+				<Routes>
+					<Route exact path='/' element={<ToDo token={token} />}></Route>
+					<Route path='*' element={<ToDo token={token} />}></Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
--- frontend/src/components/login-signup/Login.js
+++ frontend/src/components/login-signup/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import { handleError, clearError } from '../utils/HelperFunctions.js';
+import { handleError, clearError } from '../../utils/HelperFunctions.js';
 import { createBrowserHistory } from 'history';
 
 let history = createBrowserHistory();
