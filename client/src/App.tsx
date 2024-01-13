15c5fb798a6424bdb9bfc4675fbf1d86c46acab2
Lilla Németh <naemly@protonmail.com>
2024-01-13 18:36:33 +0200

    Deleting App.js, creating App.tsx - basic type definitions for now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,35 @@
 import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ToDo from './components/ToDo';
 import SignUp from './components/login-signup/SignUp';
-import Login from './components/login-signup/Login.tsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/login-signup/Login';
 import Navbar from './components/Navbar';
 import './styles/App.css';
 import { handleError, handleLogOut } from './utils/HelperFunctions';
 import { createOptions } from './context/RequestOptions';
 import { changeOrGetData } from './context/Requests';
 
-function App() {
-	const [token, setToken] = useState(null);
+const App = () => {
+	const [token, setToken] = useState<string | null>(null);
 	const [user, setUser] = useState('');
 	const [errorMsg, setErrorMsg] = useState('');
 
-	function addUser(token) {
+	function addUser(token: any) {
 		const options = createOptions('get', '/user', 'cors', 'application/json', token, null);
 
 		changeOrGetData({
 			options,
-			successCb: (res) => {
+			successCb: (res: any) => {
 				setUser(res.data);
 			},
-			errorCb: (err) => {
+			errorCb: (err: any) => {
 				handleError(err, setErrorMsg);
 			},
 		});
 	}
 
 	useEffect(() => {
-		let tokenFromLocalStorage = localStorage.getItem('token');
+		const tokenFromLocalStorage: string | null = localStorage.getItem('token');
 
 		if (tokenFromLocalStorage) {
 			setToken(tokenFromLocalStorage);
@@ -56,12 +56,12 @@ function App() {
 			<BrowserRouter>
 				<Navbar handleLogOut={handleLogOut} setToken={setToken} user={user} />
 				<Routes>
-					<Route exact path='/' element={<ToDo token={token} />}></Route>
+					<Route path='/' element={<ToDo token={token} />}></Route>
 					<Route path='*' element={<ToDo token={token} />}></Route>
 				</Routes>
 			</BrowserRouter>
 		</>
 	);
-}
+};
 
 export default App;
