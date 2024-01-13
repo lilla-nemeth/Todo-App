c7fb0e83aedbd6e4fc1c57916f93a09b6ad3ac32
Lilla Németh <naemly@protonmail.com>
2024-01-13 21:12:16 +0200

    creating Message interface, testing in SignUp.tsx

--- client/src/components/login-signup/SignUp.tsx
+++ client/src/components/login-signup/SignUp.tsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { handleError, clearError } from '../../utils/HelperFunctions';
 import { createOptions } from '../../context/RequestOptions';
 import { changeOrGetData } from '../../context/Requests';
+import { Message } from '../../types/types';
 
 const SignUp = () => {
 	const [email, setEmail] = useState<string>('');
@@ -26,9 +27,10 @@ const SignUp = () => {
 			changeOrGetData({
 				options,
 				successCb: (res: any) => {
+					const message: Message['msg'] = res.data.msg;
 					setLoading(false);
 					setErrorMsg('');
-					setSuccessMsg(res.data.msg);
+					setSuccessMsg(message);
 					setTimeout(() => {
 						setSuccessMsg('');
 						setEmail('');
