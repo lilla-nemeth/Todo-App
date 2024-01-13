06ba08f31d216d441bebf85b89701d42db34bbab
Lilla Németh <naemly@protonmail.com>
2024-01-14 01:25:25 +0200

    Creating TodoItem; TokenData and MessageData in interfaces

--- client/src/types/interfaces.ts
+++ client/src/types/interfaces.ts
@@ -5,3 +5,22 @@ export interface Token {
 export interface Message {
 	msg: string;
 }
+
+export interface TokenData {
+	[key: string]: Token;
+}
+
+export interface MessageData {
+	[key: string]: Message;
+}
+
+export interface TodoItem {
+	id: number;
+	userid: number;
+	title: string;
+	importance: number;
+	completed: boolean;
+	created: string;
+}
+
+export interface TodoItem extends Array<TodoItem> {}
