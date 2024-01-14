f158b409c03f0e144fcc93468a4b75e2b93adcec
Lilla Németh <naemly@protonmail.com>
2024-01-14 17:39:10 +0200

    adding generateId, handleLogOut and createFormattedDate functions to helperFunctions.ts

--- client/src/utils/helperFunctions.ts
+++ client/src/utils/helperFunctions.ts
@@ -1,5 +1,29 @@
 import { Dispatch, SetStateAction, ChangeEvent } from 'react';
+import { TodoItem } from '../types/interfaces';
 
 export const handleInputChange = (stateSetter: Dispatch<SetStateAction<string>>, event: ChangeEvent<HTMLInputElement>) => {
 	stateSetter(event.target.value);
+};
+
+export const generateId = (num: string) => {
+	const number = Number(num);
+
+	if (number === 1) {
+		return 'notimportant';
+	} else if (number === 2) {
+		return 'important';
+	} else {
+		return 'urgent';
+	}
+};
+
+export const handleLogOut = (stateSetter: Dispatch<SetStateAction<string | null>>) => {
+	localStorage.removeItem('token');
+	stateSetter(null);
+};
+
+export const createFormattedDate = (Sugar: sugarjs.Sugar, el: TodoItem, dateFormat: string) => {
+	const date = Sugar.Date.create(el.created);
+	const formattedDate = Sugar.Date.format(date, dateFormat);
+	return formattedDate;
 };
