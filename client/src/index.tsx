12ed6b47f3eb908446bd0f2c0f406d7613330f73
Lilla Németh <naemly@protonmail.com>
2024-01-17 18:19:26 +0200

    Removing StateContext for now as that should be well-thought-ot (its hooks, functions, etc.)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import StateContextProvider from './context/StateContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
 	<React.StrictMode>
-		<StateContextProvider>
-			<App />
-		</StateContextProvider>
+		<App />
 	</React.StrictMode>
 );
