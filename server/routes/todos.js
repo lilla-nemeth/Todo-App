bf82a05932e16a18d5d907b87949277681482238
Lilla Németh <naemly@protonmail.com>
2024-02-26 17:53:52 +0200

    removing lines

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -5,13 +5,11 @@ const { getAllTodos, getTodo, createTodo, deleteTodo, deleteAllTodos, updateTodo
 const router = Router();
 
 router.get('/', authMw, getAllTodos);
-
 router.get('/:id', authMw, getTodo);
 
 router.post('/', authMw, createTodo);
 
 router.delete('/:id', authMw, deleteTodo);
-
 router.delete('/', authMw, deleteAllTodos);
 
 router.put('/:id', authMw, updateTodo);
