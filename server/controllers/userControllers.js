9ca4f7ab7463ddfa8a42ae664eca9b80e7108f6d
Lilla Németh <naemly@protonmail.com>
2024-03-21 17:19:12 +0200

    userControllers with async/await

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -6,49 +6,41 @@ const login = async (request, response) => {
 	const email = request.body.email;
 	const pw = request.body.pw;
 
-	await pool
-		.query('SELECT * FROM users WHERE email=$1', [email])
-		.then((res) => {
-			const encryptedPw = res.rows[0].pw;
-			res.rows &&
-				bcrypt.compare(pw, encryptedPw).then((isMatch) => {
-					if (isMatch === true) {
-						jwt.sign({ id: res.rows[0].id }, 'nomoresecret', (err, token) => {
-							const currentUser = {};
+	try {
+		const res = await pool.query('SELECT * FROM users WHERE email=$1', [email]);
+		const encryptedPw = res.rows[0].pw;
+		const isMatch = await bcrypt.compare(pw, encryptedPw);
 
-							currentUser.token = token;
-							response.status(200).json(currentUser);
-						});
-					} else {
-						response.status(403).json({ msg: "Passwords don't match" });
-					}
-				});
-		})
-		.catch((err) => {
-			console.log(err);
-			response.status(400).json({ msg: 'User not found' });
-		});
+		if (isMatch) {
+			const token = jwt.sign({ id: res.rows[0].id }, 'nomoresecret');
+			const currentUser = { token };
+			response.status(200).json(currentUser);
+		} else {
+			response.status(403).json({ msg: "Passwords don't match" });
+		}
+	} catch (error) {
+		console.log(error);
+		response.status(400).json({ msg: 'User not found' });
+	}
 };
 
 const signUp = async (request, response) => {
 	const email = request.body.email;
 	const username = request.body.username;
 	const pw = request.body.pw;
-	const encryptedPw = bcrypt.hashSync(pw, 10);
+	const encryptedPw = await bcrypt.hash(pw, 10);
 
-	await pool
-		.query('INSERT INTO users (email, username, pw) VALUES ($1, $2, $3) RETURNING *', [email, username, encryptedPw])
-		.then((res) => {
-			response.status(200).json({ msg: 'User successfully created' });
-		})
-		.catch((err) => {
-			console.log(err);
-			if ((err.code = '23505')) {
-				response.status(403).json({ msg: 'User already exists' });
-			} else {
-				response.status(401).json({ msg: 'Failed to create user' });
-			}
-		});
+	try {
+		await pool.query('INSERT INTO users (email, username, pw) VALUES ($1, $2, $3) RETURNING *', [email, username, encryptedPw]);
+		response.status(200).json({ msg: 'User successfully created' });
+	} catch (error) {
+		console.log(error);
+		if (error.code === '23505') {
+			response.status(403).json({ msg: 'User already exists' });
+		} else {
+			response.status(401).json({ msg: 'Failed to create user' });
+		}
+	}
 };
 
 module.exports = { login, signUp };
